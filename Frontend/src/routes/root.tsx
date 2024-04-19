3c5824d6cbca3e3d59e8565fe752c7e3385f3078
speravi <91125469+speravi@users.noreply.github.com>
2024-04-19 10:15:26 +0300

    smaller navbar, add enlarge image

--- Frontend/src/routes/root.tsx
+++ Frontend/src/routes/root.tsx
@@ -4,7 +4,7 @@ export default function Root() {
   return (
     <div className="">
       <div className="text-foreground sticky top-0 z-20 backdrop-blur-lg">
-        <nav className="w-9/12 m-auto py-6 flex justify-between">
+        <nav className="w-9/12 m-auto py-2 flex justify-between">
           <h1 className="font-bold text-3xl">
             <Link to={"/"}> PICTRU</Link>
           </h1>
