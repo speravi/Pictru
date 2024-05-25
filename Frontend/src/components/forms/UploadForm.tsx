73165702a5a6ebb5cc9498ae99f460d92a98dc7f
speravi <91125469+speravi@users.noreply.github.com>
2024-05-25 15:30:10 +0300

    fill profile edit form with existing data, fill file upload description accordingly to selected tags

--- Frontend/src/components/forms/UploadForm.tsx
+++ Frontend/src/components/forms/UploadForm.tsx
@@ -11,15 +11,29 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { TagNames } from "@/lib/tags";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
-// import { UploadValidation } from "@/lib/validation";
 import { UploadValidation } from "@/lib/validation";
 import { useAuth } from "@/context/useAuth";
 
 interface UploadFormProps {
   onImageUpload: (id: number) => void;
 }
+
+const tagDescriptionMap = {
+  AIGenerated:
+    "AIGenearted\nModel : <Please enter the model used to generate the image>\nPrompt: <Please enter the prompt used to generate the image>",
+  Painting:
+    "Painting\nMedium: <Please enter the medium used>\nCanvas: <Please enter the canvas details>",
+  Photography:
+    "Photography\nCamera: <Please enter the camera and settings used to take the picture>",
+  DigitalArt: "DigitalArt\nSoftware: <Please enter the software used>\n",
+  Render:
+    "Render\nSoftware: <Please enter the software used>\nSettings: <Please enter the render settings>",
+  Drawing:
+    "Drawing\nMedium: <Please enter the medium used>\nPaper: <Please enter the paper details>",
+  Other: "Other\nDetails: <Please provide relevant details>",
+};
 
 export default function UploadForm({ onImageUpload }: UploadFormProps) {
   const form = useForm<z.infer<typeof UploadValidation>>({
@@ -30,22 +44,19 @@ export default function UploadForm({ onImageUpload }: UploadFormProps) {
 
   const [isLoading, setIsLoading] = useState(false);
   const [picturePreview, setPicturePreview] = useState<any>(null);
+  const [description, setDescription] = useState<string>("");
 
   const onSubmit = async (values: z.infer<typeof UploadValidation>) => {
     setIsLoading(true);
     const formData = new FormData();
-    // const token = localStorage.getItem("token");
-
     formData.append("Name", values.Name);
-    formData.append("Description", values.Description);
+    formData.append("Description", description);
 
     if (values.File) formData.append("File", values.File, values.File.name);
 
     for (const tag of values.Tags) {
       formData.append("Tags[]", tag.toString());
     }
-
-    console.log(formData);
 
     const response = await fetch("http://localhost:5095/api/image", {
       method: "POST",
@@ -64,7 +75,7 @@ export default function UploadForm({ onImageUpload }: UploadFormProps) {
     onImageUpload(res.id);
 
     form.reset({ Description: "", Name: "", Tags: [], File: undefined });
-
+    setDescription("");
     setIsLoading(false);
   };
 
@@ -74,6 +85,14 @@ export default function UploadForm({ onImageUpload }: UploadFormProps) {
     );
 
     form.setValue("Tags", enumSelection as [TagNames, ...TagNames[]]);
+    updateDescription(enumSelection as TagNames[]);
+  };
+
+  const updateDescription = (selectedTags: TagNames[]) => {
+    const newDescription = selectedTags
+      .map((tag) => tagDescriptionMap[TagNames[tag]])
+      .join("\n\n");
+    setDescription(newDescription);
   };
 
   return (
@@ -107,7 +126,7 @@ export default function UploadForm({ onImageUpload }: UploadFormProps) {
           >
             {(Object.values(TagNames) as TagNames[]).map((value) => {
               if (!isNaN(Number(value))) {
-                return;
+                return null;
               }
               return (
                 <ToggleGroupItem
@@ -127,7 +146,13 @@ export default function UploadForm({ onImageUpload }: UploadFormProps) {
               <FormItem>
                 <FormLabel>Description</FormLabel>
                 <FormControl>
-                  <Input type="text" placeholder="description" {...field} />
+                  <textarea
+                    className="h-32 w-full text-start bg-background border-border border rounded-md text-xl text-foreground p-2"
+                    placeholder="description"
+                    {...field}
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
                 </FormControl>
               </FormItem>
             )}
@@ -142,7 +167,6 @@ export default function UploadForm({ onImageUpload }: UploadFormProps) {
                 <FormLabel>Picture</FormLabel>
                 <FormControl className="flex items-center content-center absolute top-5 left-0 z-10">
                   <Input
-                    className=""
                     type="file"
                     placeholder="Select picture here"
                     {...field}
@@ -150,9 +174,7 @@ export default function UploadForm({ onImageUpload }: UploadFormProps) {
                       if (!event.target.files) return;
 
                       const file = event.target.files[0] ?? null;
-
                       onChange(file);
-
                       setPicturePreview(URL.createObjectURL(file));
                     }}
                   />
